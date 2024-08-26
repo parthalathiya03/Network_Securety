55f0f0120bbb6f046b32c02fbcaa6dce6a471159
Parth_Lathiya <chetanparth02@gmail.com>
2024-08-26 10:10:32 +0530

    config and artifact is ready

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -8,8 +8,11 @@ print(training_pipeline.ARTIFACT_DIR)
 class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
         
+        timestamp = timestamp.strftime("%m_%d_%Y_%H__%M_%S")
+        
         self.pipeline_name=training_pipeline.PIPELINE_NAME
         self.artifact_name=training_pipeline.ARTIFACT_DIR
+        
         self.artifact_dir=os.path.join(self.artifact_name,timestamp)
         self.timestamp: str=timestamp
         
--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -5,30 +5,30 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
 
 from networksecurity.components.data_ingestion import DataIngestion
-from networksecurity.components.data_validation import DataValidation
-from networksecurity.components.data_transformation import DataTransformation
-from networksecurity.components.model_trainer import ModelTrainer
-from networksecurity.components.model_evaluation import ModelEvaluation
-from networksecurity.components.model_pusher import ModelPusher
+# from networksecurity.components.data_validation import DataValidation
+# from networksecurity.components.data_transformation import DataTransformation
+# from networksecurity.components.model_trainer import ModelTrainer
+# from networksecurity.components.model_evaluation import ModelEvaluation
+# from networksecurity.components.model_pusher import ModelPusher
 
 from networksecurity.entity.config_entity import(
     TrainingPipelineConfig,
     DataIngestionConfig,
-    DataValidationConfig,
-    DataTransformationConfig,
-    ModelTrainerConfig,
-    ModelEvaluationConfig,
-    ModelPusherConfig
+    # DataValidationConfig,
+    # DataTransformationConfig,
+    # ModelTrainerConfig,
+    # ModelEvaluationConfig,
+    # ModelPusherConfig
    
 )
 
 from networksecurity.entity.artifact_entity import (
     DataIngestionArtifact,
-    DataValidationArtifact,
-    DataTransformationArtifact,
-    ModelTrainerArtifact,
-    ModelEvaluationArtifact,
-    ModelPusherArtifact
+    # DataValidationArtifact,
+    # DataTransformationArtifact,
+    # ModelTrainerArtifact,
+    # ModelEvaluationArtifact,
+    # ModelPusherArtifact
 )
 
 
@@ -49,91 +49,91 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
-        try:
-            # data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
-            # data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
-            # data_validation_artifact=data_validation.initiate_data_validation()
-            # return data_validation_artifact
-            pass
-        except Exception as e:
-            raise NetworkSecurityException(e,sys)
+    # def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
+    #     try:
+    #         # data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+    #         # data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
+    #         # data_validation_artifact=data_validation.initiate_data_validation()
+    #         # return data_validation_artifact
+    #         pass
+    #     except Exception as e:
+    #         raise NetworkSecurityException(e,sys)
         
-    def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
-        try:
-            # data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
-            # data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
-            # data_transformation_config=data_transformation_config)
+    # def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
+    #     try:
+    #         # data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+    #         # data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
+    #         # data_transformation_config=data_transformation_config)
             
-            # data_transformation_artifact = data_transformation.initiate_data_transformation()
-            # return data_transformation_artifact
-            pass
-        except Exception as e:
-            raise NetworkSecurityException(e,sys)
+    #         # data_transformation_artifact = data_transformation.initiate_data_transformation()
+    #         # return data_transformation_artifact
+    #         pass
+    #     except Exception as e:
+    #         raise NetworkSecurityException(e,sys)
     
-    def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
-        try:
-            # self.model_trainer_config: ModelTrainerConfig = ModelTrainerConfig(
-            #     training_pipeline_config=self.training_pipeline_config
-            # )
+    # def start_model_trainer(self,data_transformation_artifact:DataTransformationArtifact)->ModelTrainerArtifact:
+    #     try:
+    #         # self.model_trainer_config: ModelTrainerConfig = ModelTrainerConfig(
+    #         #     training_pipeline_config=self.training_pipeline_config
+    #         # )
 
-            # model_trainer = ModelTrainer(
-            #     data_transformation_artifact=data_transformation_artifact,
-            #     model_trainer_config=self.model_trainer_config,
-            # )
+    #         # model_trainer = ModelTrainer(
+    #         #     data_transformation_artifact=data_transformation_artifact,
+    #         #     model_trainer_config=self.model_trainer_config,
+    #         # )
 
-            # model_trainer_artifact = model_trainer.initiate_model_trainer()
+    #         # model_trainer_artifact = model_trainer.initiate_model_trainer()
 
-            # return model_trainer_artifact
-            pass
+    #         # return model_trainer_artifact
+    #         pass
 
-        except Exception as e:
-            raise NetworkSecurityException(e, sys)
+    #     except Exception as e:
+    #         raise NetworkSecurityException(e, sys)
         
-    def start_model_evaluation(self,data_validation_artifact:DataValidationArtifact,
-                                 model_trainer_artifact:ModelTrainerArtifact,):
-        try:
-            # model_evaluation_config:ModelEvaluationConfig=ModelEvaluationConfig(training_pipeline_config=self.training_pipeline_config)
-            # model_eval=ModelEvaluation(model_evaluation_config,data_validation_artifact,model_trainer_artifact)
-            # model_eval_artifact=model_eval.initiate_model_evaluation()
-            # return  model_eval_artifact
-            pass
+    # def start_model_evaluation(self,data_validation_artifact:DataValidationArtifact,
+    #                              model_trainer_artifact:ModelTrainerArtifact,):
+    #     try:
+    #         # model_evaluation_config:ModelEvaluationConfig=ModelEvaluationConfig(training_pipeline_config=self.training_pipeline_config)
+    #         # model_eval=ModelEvaluation(model_evaluation_config,data_validation_artifact,model_trainer_artifact)
+    #         # model_eval_artifact=model_eval.initiate_model_evaluation()
+    #         # return  model_eval_artifact
+    #         pass
 
-        except Exception as e:
-            raise NetworkSecurityException(e,sys)
+    #     except Exception as e:
+    #         raise NetworkSecurityException(e,sys)
         
-    def start_model_pusher(self,model_eval_artifact:ModelEvaluationArtifact):
-        try:
-            # model_pusher_config = ModelPusherConfig(training_pipeline_config=self.training_pipeline_config)
-            # model_pusher = ModelPusher(model_pusher_config, model_eval_artifact)
-            # model_pusher_artifact = model_pusher.initiate_model_pusher()
-            # return model_pusher_artifact
-            pass
-        except  Exception as e:
-            raise  NetworkSecurityException(e,sys)
+    # def start_model_pusher(self,model_eval_artifact:ModelEvaluationArtifact):
+    #     try:
+    #         # model_pusher_config = ModelPusherConfig(training_pipeline_config=self.training_pipeline_config)
+    #         # model_pusher = ModelPusher(model_pusher_config, model_eval_artifact)
+    #         # model_pusher_artifact = model_pusher.initiate_model_pusher()
+    #         # return model_pusher_artifact
+    #         pass
+    #     except  Exception as e:
+    #         raise  NetworkSecurityException(e,sys)
         
-    def run_pipeline(self):
-        try:
-            # data_ingestion_artifact=self.start_data_ingestion()
-            # #print(data_ingestion_artifact)
-            # data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
-            # #print(data_validation_artifact)
-            # data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
-            # #print(data_transformation_artifact)
+    # def run_pipeline(self):
+    #     try:
+    #         # data_ingestion_artifact=self.start_data_ingestion()
+    #         # #print(data_ingestion_artifact)
+    #         # data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+    #         # #print(data_validation_artifact)
+    #         # data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+    #         # #print(data_transformation_artifact)
             
-            # model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
-            # model_eval_artifact=self.start_model_evaluation(data_validation_artifact=data_validation_artifact,model_trainer_artifact=model_trainer_artifact)
-            # if not model_eval_artifact.is_model_accepted:
-            #     raise Exception("Trained model is not better than the best model")
-            # print(model_eval_artifact)
+    #         # model_trainer_artifact=self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+    #         # model_eval_artifact=self.start_model_evaluation(data_validation_artifact=data_validation_artifact,model_trainer_artifact=model_trainer_artifact)
+    #         # if not model_eval_artifact.is_model_accepted:
+    #         #     raise Exception("Trained model is not better than the best model")
+    #         # print(model_eval_artifact)
             
-            # model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
-            pass
+    #         # model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
+    #         pass
             
-            #TrainingPipeline.is_pipeline_running=False
-            #self.sync_artifact_dir_to_s3()
-            #self.sync_saved_model_dir_to_s3()
-        except Exception as e:
-            raise NetworkSecurityException(e,sys)
+    #         #TrainingPipeline.is_pipeline_running=False
+    #         #self.sync_artifact_dir_to_s3()
+    #         #self.sync_saved_model_dir_to_s3()
+    #     except Exception as e:
+    #         raise NetworkSecurityException(e,sys)
         
     
