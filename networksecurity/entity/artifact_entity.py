48657d6ad84667b334428283ecad353e1f12b154
Parth_Lathiya <chetanparth02@gmail.com>
2024-08-27 15:18:52 +0530

    data_validation_artifacts is done

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -7,4 +7,9 @@ class DataIngestionArtifact:
 
 @dataclass
 class DataValidationArtifacts:
-    pass
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
