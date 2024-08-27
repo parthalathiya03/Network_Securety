1e9fe689c08545435fc3fe998c3b11a6083f6b59
Parth_Lathiya <chetanparth02@gmail.com>
2024-08-27 20:46:37 +0530

    data_transformation structure is ready

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -6,18 +6,20 @@ from networksecurity.logger.logger import logging
 
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_validation import DataTransformation
 
 
 from networksecurity.entity.config_entity import(
     TrainingPipelineConfig,
     DataIngestionConfig,
-    DataValidationConfig
+    DataValidationConfig,
+    Data_TransformationConfig
 )
 
 from networksecurity.entity.artifact_entity import (
     DataIngestionArtifact,
-    DataValidationArtifact
-    
+    DataValidationArtifact,
+    DataTransformationArtifact
 )
 
 
@@ -42,15 +44,26 @@ class TrainingPipeline:
     def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
         try:
             data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
-            data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=data_validation_config)
+            
+            data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                           data_validation_config=data_validation_config)
             data_validation_artifact=data_validation.initiate_data_validation()
+            
             return data_validation_artifact
+        
         except Exception as e:
             raise NetworkSecurityException(e,sys)
     
     def start_data_transformation(self):
         try:
-            pass
+            data_transformation_config = DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_transformation = DataTransformation(data_validation_artifact=data_validation_artifact,
+                                                     data_transformation_config=data_transformation_config)
+            
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            
+            return data_transformation_artifact
+        
         except Exception as e:
             raise NetworkSecurityException(e,sys)
      
@@ -77,7 +90,11 @@ class TrainingPipeline:
             data_ingestion_artifact=self.start_data_ingestion() 
             print(data_ingestion_artifact)
             
-            data_validation = self.start_data_validation(data_ingestion_artifact)
-            print(data_validation)
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
+            print(data_validation_artifact)
+            
+            data_transformation_artifact = self.data_transformation(data_validation_artifact)
+            print(data_transformation_artifact)
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)    
